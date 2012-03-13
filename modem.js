bb50b265353223368052e8b71130d9001b233999
Niklas Schmücker <nschmuecker@gmail.com>
2012-03-13 14:56:28 +0100

    Read from stdin

--- modem.js
+++ modem.js
@@ -9,9 +9,15 @@ var fs = require('fs')
 var args = proc.argv.slice(3)
   , exec = proc.argv[2]
   , streams
+  , cmd
 
-cmd = cp.spawn(exec, args, { cwd: process.env.CWD })
-streams = [cmd.stdout, cmd.stderr]
+if (proc.argv.length > 2) {
+  cmd = cp.spawn(exec, args, { cwd: proc.env.CWD })
+  streams = [cmd.stdout, cmd.stderr]
+} else {
+  streams = [proc.stdin]
+  proc.stdin.resume()
+}
 
 streams.forEach(function (stream) {
   stream.on('data', function (data) {
