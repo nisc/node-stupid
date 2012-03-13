f65d4396e2968c10113be6c53646f2df0ce4ee85
Niklas Schmücker <nschmuecker@gmail.com>
2012-03-13 14:25:46 +0100

    Initial

---
+++ modem.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+var fs = require('fs')
+  , cp = require('child_process')
+  , proc = process
+  , AVG_DELAY_MS = 150
+  , CHUNK = 25
+
+var args = proc.argv.slice(3)
+  , exec = proc.argv[2]
+
+cmd = cp.spawn(exec, args, { cwd: __dirname })
+
+cmd.stdout.on('data', function (data) {
+  cmd.stdout.pause()
+  var offset = 0
+    , end = data.length
+  ;
+  (function writeChunk () {
+    proc.stdout.write(data.slice(offset, Math.min(offset+CHUNK, end)))
+    if ((offset += CHUNK) < end) {
+      setTimeout(writeChunk, (.5 + Math.random()) * AVG_DELAY_MS)
+    } else {
+      cmd.stdout.resume()
+    }
+  }).call()
+})
